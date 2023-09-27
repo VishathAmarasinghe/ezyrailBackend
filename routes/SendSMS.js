0848aadb739d907beaa91b633bd42798a948f701
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2023-09-27 14:30:59 +0530

    way updated

--- routes/SendSMS.js
+++ routes/SendSMS.js
@@ -6,26 +6,33 @@ const axios = require("axios");
 router.post("/SMS", async (req, res) => {
   try {
     // console.log("request body "+req.body);
-    axios
-      .post("https://api.mspace.lk/sms/send", {
+    const response=await axios.post("https://api.mspace.lk/sms/send",{
         version: "1.0",
         applicationId: "APP_008044",
         password: "APP_008044",
         message: "Hello world",
         destinationAddresses: ["tel:94711923774"],
-      })
-      .then(function (response) {
-        // console.log("axios response "+response);
-        res.status(200).json(response);
-        // res.send(response);
-      })
-      .catch(function (error) {
-        console.log("axios Error "+error);
-      })
+    });
+    // axios
+    //   .post("https://api.mspace.lk/sms/send", {
+    //     version: "1.0",
+    //     applicationId: "APP_008044",
+    //     password: "APP_008044",
+    //     message: "Hello world",
+    //     destinationAddresses: ["tel:94711923774"],
+    //   })
+    //   .then(function (response) {
+    //     // console.log("axios response "+response);
+    //     res.status(200).json(response);
+    //     // res.send(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log("axios Error "+error);
+    //   })
 
     
   } catch (error) {
-    // res.status(500).json("Error Occored"+error);
+    res.status(500).json("Error Occored "+error);
   }
 });
 
