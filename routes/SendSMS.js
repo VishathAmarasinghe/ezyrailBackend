59598f2d1e669903c5b89fb42566b43cfe881864
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2023-09-27 12:50:08 +0530

    new update2

--- routes/SendSMS.js
+++ routes/SendSMS.js
@@ -1,24 +1,18 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
+const axios = require("axios");
 
-const axios = require('axios');
-
-
-
-
-router.post("/SMS",async (req,res)=>{
-    axios.post('https://api.mspace.lk/sms/send', 
-         {
-            "version": "1.0",
-            "applicationId": "APP_008044",
-            "password": "ezyrail_123",
-            "message": "Hello world",
-            "destinationAddresses": [
-            "tel:94711923774"
-            ]
-            
-        })
+router.post("/SMS", async (req, res) => {
+  try {
+    axios
+      .post("https://api.mspace.lk/sms/send", {
+        version: "1.0",
+        applicationId: "APP_008044",
+        password: "ezyrail_123",
+        message: "Hello world",
+        destinationAddresses: ["tel:94711923774"],
+      })
       .then(function (response) {
         console.log(response);
       })
@@ -28,8 +22,12 @@ router.post("/SMS",async (req,res)=>{
       .finally(function () {
         console.log("finished SuccessFully");
         // always executed
-      });  
-})
+      });
 
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-module.exports=router;
+module.exports = router;
