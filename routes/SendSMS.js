95b72a8b8918bbba9083f333e3c35a39debac3f3
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2023-09-28 05:24:01 +0530

    re

--- routes/SendSMS.js
+++ routes/SendSMS.js
@@ -6,23 +6,23 @@ const axios = require("axios");
 
 router.post("/SMS", async (req, res) => {
   try {
-    // console.log("request body "+req.body);
     const response=await axios.post("https://api.mspace.lk/sms/send",{
         "version": "1.0",
         "applicationId": "APP_008048",
         "password": "892157f1e36be1cc1f7e75dd474db03c",
-        "message": "Hello world new",
-        "destinationAddresses": ["tel:94711923774"],
-        "sourceAddress": "77011",
-"deliveryStatusRequest": "1",
-"encoding": "245",
-"binaryHeader": "526574697265206170706c69636174696f6e20616e642072656c6561736520524b7320696620666f756e642065787069726564"
+        "message": "Hello world",
+        "destinationAddresses": ["tel:94711923774"]
     });
 
-    response.a=response;
-    stringify(response);
-    // res.status(200).send(response);
-    console.log(response);
+
+
+
+
+    
+    // response.a=response;
+    // stringify(response);
+    res.status(200).send(response);
+    // console.log(response);
     // axios
     //   .post("https://api.mspace.lk/sms/send", {
     //     version: "1.0",
