aeac1e1645975dd1eedc996e14b8f3f690c62a41
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2023-09-28 11:19:53 +0530

    added cash thing

--- routes/SendSMS.js
+++ routes/SendSMS.js
@@ -17,37 +17,19 @@ router.post("/SMS", async (req, res) => {
     // response.a=response;
     // stringify(response);
     res.status(200).send(response);
-    // console.log(response);
-    // axios
-    //   .post("https://api.mspace.lk/sms/send", {
-    //     version: "1.0",
-    //     applicationId: "APP_008044",
-    //     password: "APP_008044",
-    //     message: "Hello world",
-    //     destinationAddresses: ["tel:94711923774"],
-    //   })
-    //   .then(function (response) {
-    //     // console.log("axios response "+response);
-    //     res.status(200).json(response);
-    //     // res.send(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log("axios Error "+error);
-    //   })
   } catch (error) {
     res.status(500).json("Error Occored " + error);
   }
 });
 
 router.post("/OTP", async (req, res) => {
-    
   try {
-    const phoneNo=req.body.phoneNo;
+    const phoneNo = req.body.phoneNo;
     console.log(phoneNo);
     const response = await axios.post("https://api.mspace.lk/otp/request", {
       applicationId: "APP_008048",
       password: "892157f1e36be1cc1f7e75dd474db03c",
-      subscriberId: "tel:"+phoneNo,
+      subscriberId: "tel:" + phoneNo,
       applicationMetaData: {
         client: "MOBILEAPP",
         device: "Redmi 9T",
@@ -65,16 +47,40 @@ router.post("/OTP", async (req, res) => {
 
 router.post("/OTPres", async (req, res) => {
   try {
-    const refNo=req.body.referenceNo;
-    const otpNo=req.body.OTP;
-    console.log("ref no "+refNo);
-    console.log("otpNo "+otpNo);
+    const refNo = req.body.referenceNo;
+    const otpNo = req.body.OTP;
+    console.log("ref no " + refNo);
+    console.log("otpNo " + otpNo);
     const response = await axios.post("https://api.mspace.lk/otp/verify", {
       applicationId: "APP_008048",
       password: "892157f1e36be1cc1f7e75dd474db03c",
       referenceNo: refNo,
       otp: otpNo,
     });
+
+    // response.a=response;
+    // stringify(response);
+    console.log(response.data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json("Error Occored " + error);
+  }
+});
+
+router.post("/cashtrans", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.mspace.lk/caas/direct/debit",
+      {
+        applicationId: "APP_008048",
+        password: "892157f1e36be1cc1f7e75dd474db03c",
+        externalTrxId: "12345678901234567890123456789012",
+        subscriberId: "tel:94711923774",
+        paymentInstrumentName: "Mobile Account",
+        amount: "2",
+        currency: "LKR",
+      }
+    );
 
     // response.a=response;
     // stringify(response);
