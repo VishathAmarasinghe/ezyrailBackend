052f8ede63f8ea595611bfeaf52cf829c60742e5
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2023-09-28 11:00:58 +0530

    find registration Error

--- routes/auth.js
+++ routes/auth.js
@@ -46,6 +46,7 @@ router.post("/register",async(req,res)=>{
         res.status(200).json(user);
         
      } catch (error) {
+        console.log("user Adding Error "+error);
         res.status(500).json(error);
      }
 })
